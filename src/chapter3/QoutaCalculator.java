e368be51ea5fffaac39ba70e129ef0ec2fe5b7a4
Charles Mbonani <65178528+ProCharles@users.noreply.github.com>
2025-02-12 14:28:55 +0200

    second tutorial

---
+++ src/chapter3/QoutaCalculator.java
@@ -0,0 +1,26 @@
+package chapter3;
+
+import java.util.Scanner;
+
+//if else
+public class QoutaCalculator {
+    public  static  void  main (String args[]){
+        // intialize  known values
+        int qouta =10;
+        //Get unknown values
+        System.out.println("Enter the numbers of sales you made this week:  ");
+        Scanner scanner = new Scanner(System.in);
+        int sales =scanner.nextInt();
+        scanner.close();
+
+        //make a decision
+        if(sales>=qouta) {
+            System.out.println("Congrats you have met you qouta");
+        }
+        else {
+            int salesShort  =qouta - sales;
+            System.out.println("you did not meet your qouat . You were " + salesShort + " Sales short ");
+        }
+    }
+}
+
